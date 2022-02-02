575164a2de7ba17203e32bfdd9f30fbbe786f528
Mostafa Ahangarha <ahangarha@riseup.net>
2022-02-02 14:38:20 +0300

    add speakers dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,48 @@ menuLinks.forEach((link) => link.addEventListener(
   'click',
   (event) => toggleMenu(event, false),
 ));
+
+// Dynamic Speakers insertion
+const speakers = [
+  {
+    name: 'Mostafa',
+    title: 'Software Freedom Activist',
+    image: 'person1.jpg',
+    bio: 'I support Software Freedom as it is the only way to achieve justice in IT.',
+  },
+  {
+    name: 'Hanna',
+    title: 'Digital Painter',
+    image: 'person2.jpg',
+    bio: 'some bio',
+  },
+  {
+    name: 'Reza',
+    title: 'Lawer',
+    image: 'person3.jpg',
+    bio: 'some other bio',
+  },
+  {
+    name: 'Shirin',
+    title: 'Laravel Developer',
+    image: 'person4.jpg',
+    bio: 'some other bio',
+  },
+];
+
+const speakersPlaceHolder = document.querySelector('#speakers .persons');
+
+speakers.forEach((speaker) => {
+  speakersPlaceHolder.innerHTML += `
+  <div class="person">
+    <div class="person-graphic">
+      <img src="./assets/img/${speaker.image}" alt="" />
+    </div>
+    <div class="person-info">
+      <h4 class="person-name">${speaker.name}</h4>
+      <p class="person-title"><em>${speaker.title}</em></p>
+      <p class="person-bio">${speaker.bio}</p>
+    </div>
+  </div>
+  `;
+});
